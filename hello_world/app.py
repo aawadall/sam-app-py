f90836eae06587369960e7841c14b563a8557f93
Ahmed Awadallah <aawadall@ualberta.ca>
2019-02-28 16:55:53 -0500

    could not load context

--- hello_world/app.py
+++ hello_world/app.py
@@ -33,11 +33,9 @@ def lambda_handler(event, context):
 
     #     raise e
 
-    event_string = json.dumps(event, indent=4)
-        #.replace('\"',"").replace("u\'","\'").replace(" \/","")
+
     body = {
         "event": event,
-        "context": context
     }
     ret =  {
         "statusCode": 200,
