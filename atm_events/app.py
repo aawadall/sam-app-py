2a7bdfadedd3fb8c434920ea895a1e178a1c7599
Ahmed Awadallah <aawadall@ualberta.ca>
2019-03-01 11:13:29 -0500

    attempt to print

--- atm_events/app.py
+++ atm_events/app.py
@@ -4,12 +4,12 @@ def event_handler(event, context):
     body = {
         "event": event,
     }
+
+    print("log_group_name: {}",context.log_group_name)
+    print("log_stream_name: {}", context.log_stream_name)
+
     ret = {
         "statusCode": 200,
-        "log": {
-            "log_group_name": context.log_group_name,
-            "log_stream_name": context.log_stream_name,
-        },
         "body": json.dumps(body),
     }
 
