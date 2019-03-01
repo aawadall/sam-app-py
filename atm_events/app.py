45a4b6c3c6d5ba61b0d32452c7f5b814f4af4210
Ahmed Awadallah <aawadall@ualberta.ca>
2019-03-01 11:21:33 -0500

    added logger

--- atm_events/app.py
+++ atm_events/app.py
@@ -1,12 +1,16 @@
 import json
+import logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 def event_handler(event, context):
+
     body = {
         "event": event,
     }
 
-    print("log_group_name: {}",context.log_group_name)
-    print("log_stream_name: {}", context.log_stream_name)
+    logger.info("log_group_name: {}".format(context.log_group_name))
+    logger.info("log_stream_name: {}".format(context.log_stream_name))
 
     ret = {
         "statusCode": 200,
